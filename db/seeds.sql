61391106c5bfeebeb219eeab94614e7fe457d981
Shirley <90633395+Sasley@users.noreply.github.com>
2023-09-05 12:20:54 -0400

    stated table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,8 @@
 -- data from user
+USE employeesDB
+INSERT INTO department (name)
+VALUES ("ground handling"), ("gate handling"), ("customer service");
+
+-- /roles
+INSERT INTO role (title, salary, department_id)
+VALUES ("agent", "25000", 1), ("gate agent", "45000", 2)
